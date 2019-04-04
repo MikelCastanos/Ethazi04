70d2ed5de660f081d975ba55684f96ceb73802d0
ioritz101 <ibilbao@omega-peripherals.com>
2019-04-04 19:21:41 +0200

    conexion bbdd

---
+++ src/Modelo/Conectar.java
@@ -0,0 +1,25 @@
+
+package Modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class Conectar { 
+    
+    Connection conectar = null;
+    public Connection conexion(){
+   
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection("jdbc:mysql://localhost/hotel","root","");
+            System.out.println("Conexion establecida");
+           
+        }
+        
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return conectar;
+    }
+}

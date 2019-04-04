8b9ae16437fe2cd26ed0ec2334d75ec4955c2e42
MikelCastanos <mikel.castanosgo@elorrieta-errekamari.com>
2019-04-05 01:09:37 +0200

    Arreglada cagada de IraiDAW

--- src/Aplicacion/Main.java
+++ src/Aplicacion/Main.java
@@ -12,13 +12,13 @@ import javax.swing.SwingUtilities;
 public class Main {
     
     public static void main(String[] args){
-<<<<<<< HEAD
+
     Conectar cxn = new Conectar();
     cxn.conexion();
     
 //    ControladorVista controladorv=new ControladorVista();
 //    controladorv.ventanaBienvenida();
-=======
+
     //Conectar cxn = new Conectar();
     //cxn.conexion();
     VistaBienvenida vistab=new VistaBienvenida();
@@ -34,7 +34,7 @@ public class Main {
             ControladorVista controlador=new ControladorVista();
             controlador.contol();
         }});
->>>>>>> ba8545a277a761fdf5a4aeca5305942aa900f6b1
+
     
     
     }}

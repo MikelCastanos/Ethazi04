11cc3592629279d9c3036489283d8492e61e0e3a
Iraide <arrizabalagairaide@gmail.com>
2019-05-15 12:41:31 +0200

    Borrar

--- src/Modelo/Habitacion.java
+++ src/Modelo/Habitacion.java
@@ -79,7 +79,7 @@ public static Habitacion habitacion3=new Habitacion();
     
     public void coincidencia(){
         
-        habitaciones(getCama_nino(),getCama_simple(),getCama_doble(),Hotel.hotel1.getCodigoHotel());
+//        habitaciones(getCama_nino(),getCama_simple(),getCama_doble(),Hotel.hotel1.getCodigoHotel());
     }
     
     public static void comprobarDisponibilidad(){
@@ -91,34 +91,12 @@ public static Habitacion habitacion3=new Habitacion();
 //        setDisponibilidad(true);    
 //        }
     }
-    
-    
-    public void habitaciones(int camaInfantil,int camaSimple, int camaDoble, int codHotel){
-        Conexion conexion= new Conexion();
-        Consultas consultas= new Consultas();
 
-        
-        String query="select cama_infantil, cama_simple, cama_doble, precio_habitacion, cod_habitacion from habitacion "
-                + "where (cod_hotel=" +codHotel+ ")AND(cama_doble="+camaDoble +")AND(cama_simple="+camaSimple+" )AND(cama_infantil="+camaInfantil+");";
-        ResultSet rs= consultas.consultaBD(query);
-        try{
-            while(rs.next()){
-                System.out.println ("Habitacion que coincide con las camas seleccionadas :"+rs.getInt (1) + " " + rs.getInt (2)+ " " + rs.getInt(3)+ " " + rs.getDouble (4)+ " " + rs.getInt (5));
-             habitacion1.setPrecio_habitacion(rs.getDouble (4));   
-             habitacion1.setCod_habitacion(rs.getInt(5));
-            }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
     
     public void  habitacionesDisponibles(){
         Conexion conexion= new Conexion();
         Consultas consultas= new Consultas();
 
-        
-//        String query="select if (exists (select cod_habitacion, fecha_entrada, fecha_salida from reserva where "
-//                + "(cod_habitacion="+Habitacion.getCod_habitacion()+")AND(fecha_entrada='"+Alojamiento.getFechaEntrada()+"')AND(fecha_entrada='"+Alojamiento.getFechaSalida()+"')));";
         String query="select cod_habitacion, fecha_entrada, fecha_salida from reserva where "
                 + "(cod_habitacion="+getCod_habitacion()+")AND(fecha_entrada='"+Hotel.hotel1.getFechaEntrada()+"')AND(fecha_salida='"+Hotel.hotel1.getFechaSalida()+"')";
         System.out.println(query);
